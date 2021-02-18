806be1886da70dbc416ff84c0d4f52086285d907
Jeroen Peters <jeroen.peters@yourhosting.nl>
2021-02-18 22:23:25 +0100

    Travis tests pytest-django

--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -18,7 +18,7 @@ class MigrationTestCase(TestCase):
         }
 
         try:
-            call_command('makemigrations', **options)
+            call_command('makemigrations djangocms_youtube_lazyload', **options)
         except SystemExit as e:
             status_code = str(e)
         else:
