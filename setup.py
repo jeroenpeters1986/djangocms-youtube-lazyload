519cf4c3b72caab04adbed6cb96e4e21702c07d9
Jeroen Peters <jeroen.peters@yourhosting.nl>
2021-02-18 08:52:38 +0100

    Travis tests pytest-django

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     url='https://github.com/jeroenpeters1986/djangocms-youtube-lazyload',
     include_package_data=True,
     install_requires=[
+        'Django<2',
         'django-appconf',
         'django-cms>=3.0',
         'isodate>=0.5.4',
